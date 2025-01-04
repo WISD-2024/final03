b3316f3f458ce56a6e86a5933033f2e1413a04da
黃鐙霆 <7iuy6u@gmail.com>
2025-01-04 22:31:13 +0800

    更正

--- database/migrations/2025_01_04_083536_create_customers_table.php
+++ database/migrations/2025_01_04_083536_create_customers_table.php
@@ -8,22 +8,26 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->id();//顧客編號
-            $table->timestamps();
-            $table->string('name',10);//顧客名稱
-            $table->string('account',20);//顧客帳號
-            $table->string('password',20);//顧客密碼
+                $table->id();//顧客編號
+                $table->timestamps();
+                $table->string('name',10);//顧客名稱
+                $table->string('account',20);//顧客帳號
+                $table->string('password',20);//顧客密碼
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('customers');
     }
